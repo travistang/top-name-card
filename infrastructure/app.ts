879694355d41375800ca916e70042e0b6129d9a3
Travis Tang <travis.tang@tum.de>
2025-02-02 14:41:25 +0100

    :bricks: Rename stack

--- infrastructure/app.ts
+++ infrastructure/app.ts
@@ -6,13 +6,13 @@ import { NameCardStack } from "./lib/namecard-stack";
 const account = process.env.AWS_ACCOUNT;
 const region = process.env.AWS_REGION!;
 const acmArn = process.env.AWS_ACM_ARN!; // the ARN of the HTTPS certificate pre-configured for the custom domain
-const branchName = process.env.BRANCH_NAME || "Staging";
+const branchName = process.env.BRANCH_NAME || "staging";
 
 const app = new cdk.App();
 const stackProps = {
   publicUrl: "test.namecard.travis.engineering",
   certificateArn: acmArn,
-  stackName: `${branchName}Stack`,
+  stackName: `top-name-card-cdk-stack-${branchName}`,
 };
 new NameCardStack(app, stackProps, {
   env: { account, region },
